5a8a9f711c57f2113396d7a0a389e49689bbd806
athinellis <athinellis@gmail.com>
2023-11-07 12:16:40 +0200

    _css

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,4 @@
 //= link application.css
 //= link client.css
-//= link ../builds/application.js
 //= link_tree ../images
 //= link_tree ../builds
--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module DatatablesRails7
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.assets.enabled = false
 
     # Configuration for the application, engines, and railties goes here.
     #
